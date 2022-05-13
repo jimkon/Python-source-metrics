d10358d0fa7b35cfac83d66d62eb86042279d00b
jimkon <dkontzedakis@gmail.com>
2022-05-13 16:30:46 +0100

    path utils

---
+++ src/utils/path_utils.py
@@ -0,0 +1,50 @@
+import os
+
+from src.configs import PYTHON_FILE_EXTENSION
+
+
+def load_file_as_string(filepath):
+    with open(filepath, 'r') as f:
+        s = f.read()
+    return s
+
+
+def get_python_files_and_directories(path):
+    all_sub_filenames = [os.path.join(path, filename) for filename in os.listdir(os.path.normpath(path))]
+    directories = [filename for filename in all_sub_filenames if os.path.isdir(filename)]
+    python_files = filter_filenames_by_extension(all_sub_filenames, PYTHON_FILE_EXTENSION)
+    return python_files, directories
+
+
+def get_all_filenames_in_directory(path):
+    all_filenames = list()
+    for dirpath, dirnames, filenames in os.walk(path):
+        all_filenames += [os.path.normpath(os.path.join(dirpath, file)) for file in filenames]
+    return all_filenames
+
+
+def filter_filenames_by_extension(filenames, extension):
+    def filter_function(filename):
+        return len(filename) > len(extension) and filename[-len(extension):] == extension
+
+    filtered_filenames = [filename for filename in filenames if filter_function(filename)]
+    return filtered_filenames
+
+
+def remove_path_prefix(path, path_prefix):
+    relative_path = os.path.normpath(path.replace(path_prefix, ''))
+    clean_relative_path = os.sep.join(break_path_in_parts(relative_path))
+    return clean_relative_path
+
+
+def break_path_in_parts(path):
+    norm_path = os.path.normpath(path)
+    parts = [part for part in norm_path.split(os.sep) if len(part) > 0]
+    return parts
+
+
+def dotted_repr_of_path(path):
+    parts = break_path_in_parts(path)
+    dotted = '.'.join(parts)
+    return dotted
+
---
+++ tests/utils/test_path_utils.py
@@ -0,0 +1,74 @@
+import unittest
+from unittest import mock
+
+from src.utils import path_utils
+
+
+class TestFiles(unittest.TestCase):
+    def test_get_python_files_and_directories(self):
+
+        with mock.patch('os.listdir') as mock_os_listdir:
+            with mock.patch('os.path.isdir') as mock_os_isdir:
+                mock_os_isdir.side_effect = lambda x: {
+                    'test_path\\dir': True,
+                    'test_path\\a.py': False,
+                    'test_path\\b.ext': False
+                }[x]
+
+                mock_os_listdir.return_value = [
+                    'dir', 'a.py', 'b.ext'
+                ]
+                python_files, directories = path_utils.get_python_files_and_directories("test_path")
+                self.assertEqual(python_files, ['test_path\\a.py'])
+                self.assertEqual(directories, ['test_path\\dir'])
+
+    def test_get_all_filenames_in_directory(self):
+        with mock.patch('os.walk') as mock_os_walk:
+            mock_os_walk.return_value = [
+                ('.', ['directory'], ['a', 'b']),
+                ('.\\directory', [], ['c', 'd'])
+            ]
+
+            self.assertEqual(path_utils.get_all_filenames_in_directory('random_dir'),
+                             ['a', 'b', 'directory\\c', 'directory\\d'])
+
+    def test_filter_filenames_by_extension(self):
+        test_filenames = [
+            "/a/b/c",
+            "/a/b/c.py",
+            "/a/b/c.exe",
+            "d.py",
+            "\\a\b/c.py"
+        ]
+        self.assertEqual(path_utils.filter_filenames_by_extension(test_filenames, '.py'),
+                         ["/a/b/c.py", 'd.py', "\\a\b/c.py"])
+        self.assertEqual(path_utils.filter_filenames_by_extension([], '.py'), [])
+        self.assertEqual(path_utils.filter_filenames_by_extension(['a', '/b'], '.py'), [])
+
+    def test_remove_path_prefix(self):
+        self.assertEqual(path_utils.remove_path_prefix('/a/b/c/d', 'a'), 'b\\c\\d')
+        self.assertEqual(path_utils.remove_path_prefix('a\\b\\c\\d', 'a\\b'), 'c\\d')
+        self.assertEqual(path_utils.remove_path_prefix('a/b/c/d', 'a/b/c'), 'd')
+        self.assertEqual(path_utils.remove_path_prefix('a/b/c/d', 'a/b/c/d'), '.')
+
+        self.assertEqual(path_utils.remove_path_prefix('a/b/c/d', 'e/f/g'), r'a\b\c\d')
+
+    def test_break_path_in_parts(self):
+        self.assertEqual(path_utils.break_path_in_parts('a/b/c'), ['a', 'b', 'c'])
+        self.assertEqual(path_utils.break_path_in_parts('a//b//c'), ['a', 'b', 'c'])
+        self.assertEqual(path_utils.break_path_in_parts('a\\b//c'), ['a', 'b', 'c'])
+        self.assertEqual(path_utils.break_path_in_parts('a'), ['a'])
+        self.assertEqual(path_utils.break_path_in_parts('/a/'), ['a'])
+        self.assertEqual(path_utils.break_path_in_parts('/a/c.d'), ['a', 'c.d'])
+
+    def test_dotted_repr_of_path(self):
+        self.assertEqual(path_utils.dotted_repr_of_path('a/b/c'), 'a.b.c')
+        self.assertEqual(path_utils.dotted_repr_of_path('a//b//c'), 'a.b.c')
+        self.assertEqual(path_utils.dotted_repr_of_path('a\\b//c'), 'a.b.c')
+        self.assertEqual(path_utils.dotted_repr_of_path('a'), 'a')
+        self.assertEqual(path_utils.dotted_repr_of_path('/a/'), 'a')
+        self.assertEqual(path_utils.dotted_repr_of_path('/a/c.d'), 'a.c.d')
+
+
+if __name__ == '__main__':
+    unittest.main()
