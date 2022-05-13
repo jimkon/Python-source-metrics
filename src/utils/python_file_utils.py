53abca2bd65b23da1fce0ac74c0638dcff11c8e3
jimkon <dkontzedakis@gmail.com>
2022-05-13 18:24:20 +0100

    path utils

---
+++ src/utils/python_file_utils.py
@@ -0,0 +1,39 @@
+import ast
+import os
+
+from src.configs import PYTHON_FILE_EXTENSION
+from src.utils.path_utils import break_path_in_parts, filter_filenames_by_extension, get_all_filenames_in_directory, load_file_as_string, \
+    remove_path_prefix
+
+
+def validate_python_path(path):
+    _, ext = os.path.splitext(os.path.normpath(path))
+    if ext != PYTHON_FILE_EXTENSION:
+        raise ValueError(f"Path must be pointing to a python (.py) file. Given: {path}")
+
+
+def validate_and_convert_path_to_module_name(path):
+    validate_python_path(path)
+
+    filename, ext = os.path.splitext(os.path.normpath(path))
+    parts = break_path_in_parts(filename)
+    module_name = '.'.join(parts)
+
+    return module_name
+
+
+def get_all_python_files(path):
+    all_filenames = get_all_filenames_in_directory(path)
+    python_filenames = filter_filenames_by_extension(all_filenames, PYTHON_FILE_EXTENSION)
+    return python_filenames
+
+
+def load_python_modules(path):
+    result_dict = {}
+
+    for filepath in get_all_python_files(path):
+        file_content = load_file_as_string(filepath)
+        filename = remove_path_prefix(filepath, path)
+        result_dict[filename] = file_content
+    return result_dict
+
---
+++ tests/utils/test_python_file_utils.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest import mock
+
+from src.utils import python_file_utils as pu
+
+
+class TestPythonUtils(unittest.TestCase):
+
+    def test_validate_python_path(self):
+        self.assertRaises(ValueError, pu.validate_python_path, 'c')
+        try:
+            pu.validate_python_path('c.py')
+        except ValueError:
+            self.assertTrue(False)
+
+    def test_validate_and_convert_path_to_module_name(self):
+        self.assertEqual(pu.validate_and_convert_path_to_module_name("a/b/c.py"), 'a.b.c')
+        self.assertEqual(pu.validate_and_convert_path_to_module_name("c.py"), 'c')
+        self.assertRaises(ValueError, pu.validate_and_convert_path_to_module_name, 'c')
+
+    def test_get_all_python_files(self):
+        with mock.patch("src.utils.python_file_utils.get_all_filenames_in_directory") as mocker_files:
+            mocker_files.return_value = ["a", "a.py", "a/b", "a/b.extension", "a/b/c.py", "a/b/c"]
+
+            self.assertEqual(pu.get_all_python_files(''), ["a.py", "a/b/c.py"])
+
+        with mock.patch("src.utils.python_file_utils.get_all_filenames_in_directory") as mocker_files:
+            mocker_files.return_value = ["a", "a/b/d"]
+
+            self.assertEqual(pu.get_all_python_files(''), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
