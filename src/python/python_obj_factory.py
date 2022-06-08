eafd14792d6a55479cecd3b36a536db08f22751a
jimkon <dkontzedakis@gmail.com>
2022-06-08 11:57:15 +0100

    rework factory

--- src/python/python_obj_factory.py
+++ src/python/python_obj_factory.py
@@ -1,43 +1,48 @@
 import os.path
 
 from src.python.code_structure import *
-from src.utils.path_utils import remove_first_path_seperator
+from src.utils.paths import Path
 
 
 class ObjectFactory:
 
-    def __init__(self, src_dir):
-        self._source_dir = os.path.normpath(src_dir)
+    def __init__(self, root_path):
+        self._root_path = root_path
 
     @property
-    def source_path(self):
-        return self._source_dir
+    def root_path(self):
+        return self._root_path
 
-    def calc_filepath(self, filename):
-        basepath = os.path.dirname(self.source_path)
-        filepath = os.path.join(basepath, filename)
-        return filepath
+    def directory(self, name, parent=None):
+        if not parent:
+            path = self._root_path
+        else:
+            path = parent.data.path[name]
 
-    def _relative_path(self, path):
-        norm_path = os.path.normpath(path)
-        rel_path = os.path.normpath(norm_path.replace(os.path.dirname(self._source_dir), ''))
-        return remove_first_path_seperator(rel_path)
+        dir_obj = DirectoryObj(name=name, path=path)
+        return TreeNode(parent, data=dir_obj)
 
-    def directory(self, filename, parent=None):
-        return DirectoryObj(self._relative_path(filename), parent)
+    def module(self, name, code, parent=None):
+        if not parent:
+            path = self._root_path
+        else:
+            path = parent.data.path[name]
 
-    def module(self, filename, parent=None):
-        validate_python_path(filename)
-        code = load_file_as_string(filename)
-        return ModuleObj(self._relative_path(filename), code, parent)
+        module_obj = ModuleObj(name, path, code)
+        return TreeNode(parent, data=module_obj)
 
-    def class_obj(self, filename, code, parent=None):
-        return ClassObj(self._relative_path(filename), code, parent)
+    def class_node(self, name, code, parent=None):
+        path = parent.data.path
+        class_obj = ClassObj(name, path, code)
+        return TreeNode(parent, data=class_obj)
 
-    def function(self, filename, code, parent=None):
-        return FunctionObj(self._relative_path(filename), code, parent)
+    def function(self, name, code, parent=None):
+        path = parent.data.path
+        func_obj = FunctionObj(name, path, code)
+        return TreeNode(parent, data=func_obj)
 
-    def class_method(self, filename, code, parent=None):
-        return ClassMethodObj(self._relative_path(filename), code, parent)
-
+    def class_method(self, name, code, parent=None):
+        path = parent.data.path
+        class_method_obj = ClassMethodObj(name, path, code)
+        return TreeNode(parent, data=class_method_obj)
 
