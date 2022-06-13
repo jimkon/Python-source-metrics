0333eda9f197b9dd29f935463c5d5da99f8d0928
jimkon <dkontzedakis@gmail.com>
2022-06-14 00:37:42 +0100

    extract imports to csv

--- src/configs.py
+++ src/configs.py
@@ -4,6 +4,6 @@ PATH_STORE_PYTHON_SOURCE_OBJECTS = r"C:\Users\jim\PycharmProjects\Python-source-
 PATH_STORE_METRIC_RESULTS = r"C:\Users\jim\PycharmProjects\Python-source-metrics\files\metrics"
 PATH_STORE_UML_CLASS_TXT = "C:/Users/jim/PycharmProjects/Python-source-metrics/files/uml/class_uml.txt"
 PATH_STORE_UML_CLASS_RELATION_TXT = "C:/Users/jim/PycharmProjects/Python-source-metrics/files/uml/class_rel_uml.txt"
-PATH_STORE_IMPORTS_DICT = "C:/Users/jim/PycharmProjects/Python-source-metrics/files/uml/module_imports.json"
+PATH_STORE_IMPORTS_DICT = "C:/Users/jim/PycharmProjects/Python-source-metrics/files/uml/module_imports.csv"
 
 VERBOSITY = 2
--- src/reports/import_graph.py
+++ src/reports/import_graph.py
@@ -1,28 +1,27 @@
 import ast
 
+import pandas as pd
+
 from src.configs import PATH_STORE_IMPORTS_DICT
-from src.utils.storage_mixins import StoreJSON
+from src.utils.storage_mixins import StoreJSON, StoreCSV
 from src.visitors.visitor import TreeNodeVisitor
 
 
-class ImportGraphVisitor(TreeNodeVisitor, StoreJSON):
+class ImportGraphVisitor(TreeNodeVisitor, StoreCSV):
     def __init__(self):
-        self._module_imports_dict = {}
+        self._module_imports_list = []
 
     def visit_module(self, node):
-        _import_list = []
         for _ast in node.data.ast:
             if isinstance(_ast, ast.Import):
                 for alias in _ast.names:
-                    _import_list.append(alias.name)
+                    self._module_imports_list.append([node.data.name, alias.name])
             elif isinstance(_ast, ast.ImportFrom):
                 for alias in _ast.names:
-                    _import_list.append(f"{_ast.module}.{alias.name}")
-
-        self._module_imports_dict[node.data.name] = _import_list
+                    self._module_imports_list.append([node.data.name, f"{_ast.module}.{alias.name}"])
 
     def path_to_store(self):
         return PATH_STORE_IMPORTS_DICT
 
     def data_to_store(self):
-        return self._module_imports_dict
+        return pd.DataFrame(self._module_imports_list, columns=['module', 'imports'])
