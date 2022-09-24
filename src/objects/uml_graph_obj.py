202aa33c8d0ef5c9fb3a03fd89229a5071b2111c
jimkon <dkontzedakis@gmail.com>
2022-09-24 14:22:38 +0100

    moved util function out of the obj module

--- src/objects/uml_graph_obj.py
+++ src/objects/uml_graph_obj.py
@@ -1,52 +1,13 @@
-import json
-import os
 import tempfile
-import threading
 
 from src.configs import PATH_FILES_DIR
-from src.html.image_html import HTMLImageBuilder
 from src.html.pages.page import HTMLPage
 from src.objects.data_objects import AbstractObject
 from src.objects.python_object import PObject
 from src.reports.uml_class import UMLClassBuilder, UMLClassRelationBuilder
 from src.utils.file_strategies import HTMLFile
-from src.utils.logs import log_pink
-from src.utils.plantuml_utils import produce_uml_diagram_from_text_file
-
-
-def _uml_to_html(temp_dir, uml_doc):
-    temp_file = tempfile.NamedTemporaryFile(suffix="_temp_uml_text_file.txt", dir=temp_dir.name, delete=False)
-    with open(temp_file.name, 'w') as f:
-        f.write(uml_doc)
-
-    infile_name, outfile_name = temp_file.name, f"{temp_file.name}.png"
-
-    try:
-        produce_uml_diagram_from_text_file(infile_name,
-                                           output_path=outfile_name)
-        return HTMLImageBuilder(outfile_name).html
-    except Exception as e:
-        return f"<div>The following error occurred while processing the doc:" \
-               f"<br>{uml_doc}" \
-               f"<br>{e}</div>"
-
-
-class UMLImageThread(threading.Thread):
-    def __init__(self, temp_dir, uml_doc):
-        threading.Thread.__init__(self)
-        self._dir = temp_dir
-        self._uml_doc = uml_doc
-        self._result = None
-
-    def run(self):
-        import random
-        id = random.randint(0, 1000)
-        log_pink(f'Thread {id} started')
-        self._result = _uml_to_html(self._dir, self._uml_doc)
-        log_pink(f'-Thread {id} finished')
-
-    def result(self):
-        return self._result
+from src.utils.plantuml_utils import PlantUMLImageProductionThread, plantuml_doc_to_html_image, \
+    produce_plantuml_diagrams_in_html_images_multithreading, produce_plantuml_diagrams_in_html_images
 
 
 class UMLClassDiagramObj(AbstractObject):
@@ -59,29 +20,12 @@ class UMLClassDiagramObj(AbstractObject):
         uml_builder = UMLClassBuilder()
         pobj.use_visitor(uml_builder)
 
-        temp_dir = tempfile.TemporaryDirectory(dir=PATH_FILES_DIR, prefix="UMLClassDiagramObj_")
+        plantuml_doc_strings = uml_builder.result()
+
+        plantuml_diagram_html_images = produce_plantuml_diagrams_in_html_images_multithreading(plantuml_doc_strings)
+
         html_page = HTMLPage()
-
-        # ******* MULTITHREADING SOLUTION *******
-        threads = []
-        for doc in uml_builder.result():
-            thread = UMLImageThread(temp_dir, doc)
-            thread.start()
-            threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-
-        [html_page.add(thread.result()) for thread in threads]
-        # ***************************************
-
-        # ******* SEQUENTIAL EXECUTION *******
-        # for doc in uml_builder.result():
-        #     html = _uml_to_html(temp_dir, doc)
-        #     html_page.add(html)
-        # ************************************
-
-        temp_dir.cleanup()
+        [html_page.add(html_image) for html_image in plantuml_diagram_html_images]
 
         return html_page.html
 
@@ -96,11 +40,12 @@ class UMLClassRelationDiagramObj(AbstractObject):
         uml_builder = UMLClassRelationBuilder()
         pobj.use_visitor(uml_builder)
 
-        temp_dir = tempfile.TemporaryDirectory(dir=PATH_FILES_DIR, prefix="UMLClassRelationDiagramObj_")
+        plantuml_doc_strings = [uml_builder.result()]
+
+        plantuml_diagram_html_images = produce_plantuml_diagrams_in_html_images(plantuml_doc_strings)
+
         html_page = HTMLPage()
-        html = _uml_to_html(temp_dir, uml_builder.result())
-        html_page.add(html)
-        temp_dir.cleanup()
+        [html_page.add(html_image) for html_image in plantuml_diagram_html_images]
 
         return html_page.html
 
--- src/utils/plantuml_utils.py
+++ src/utils/plantuml_utils.py
@@ -1,5 +1,10 @@
+import tempfile
+import threading
+
 import plantuml
 
+from src.configs import PATH_FILES_DIR
+from src.html.image_html import HTMLImageBuilder
 from src.utils.logs import log_plantuml
 
 
@@ -7,3 +12,75 @@ def produce_uml_diagram_from_text_file(input_text_filepath, output_path):
     log_plantuml(f"Converting UML diagram image ({output_path}) from input text file ({input_text_filepath})")
     pl = plantuml.PlantUML('http://www.plantuml.com/plantuml/img/')
     pl.processes_file(input_text_filepath, outfile=output_path, directory='')
+
+
+def plantuml_doc_to_html_image(plantuml_doc, temp_dir):
+    temp_file = tempfile.NamedTemporaryFile(suffix="_temp_uml_text_file.txt", dir=temp_dir.name, delete=False)
+    with open(temp_file.name, 'w') as f:
+        f.write(plantuml_doc)
+
+    infile_name, outfile_name = temp_file.name, f"{temp_file.name}.png"
+
+    try:
+        produce_uml_diagram_from_text_file(infile_name,
+                                           output_path=outfile_name)
+        return HTMLImageBuilder(outfile_name).html
+    except Exception as e:
+        return f"<div>The following error occurred while processing the doc:" \
+               f"<br>{plantuml_doc}" \
+               f"<br>{e}</div>"
+
+
+class PlantUMLImageProductionThread(threading.Thread):
+    def __init__(self, uml_doc, _dir):
+        threading.Thread.__init__(self)
+        self._uml_doc = uml_doc
+        self._dir = _dir
+        self._result = None
+
+    def run(self):
+        import random
+        id = random.randint(0, 1000)
+        log_plantuml(f'Thread {id} started')
+        self._result = plantuml_doc_to_html_image(self._uml_doc, self._dir)
+        log_plantuml(f'-Thread {id} finished')
+
+    def result(self):
+        return self._result
+
+
+def produce_plantuml_diagrams_in_html_images(plantuml_docs):
+    temp_dir = tempfile.TemporaryDirectory(dir=PATH_FILES_DIR, prefix="temp_plantUML_images_")
+
+    html_images = []
+    for doc in plantuml_docs:
+        html = plantuml_doc_to_html_image(doc, temp_dir)
+        html_images.append(html)
+
+    temp_dir.cleanup()
+
+    return html_images
+
+
+def produce_plantuml_diagrams_in_html_images_multithreading(plantuml_docs):
+    temp_dir = tempfile.TemporaryDirectory(dir=PATH_FILES_DIR, prefix="temp_plantUML_images_")
+
+    threads = []
+
+    for doc in plantuml_docs:
+
+        thread = PlantUMLImageProductionThread(doc, temp_dir)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    html_images = [thread.result() for thread in threads]
+
+    temp_dir.cleanup()
+
+    return html_images
+
+
+
