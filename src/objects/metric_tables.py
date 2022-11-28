77b0beb866db7447f96b6d72d33cf2c42db48af9
jimkon <dkontzedakis@gmail.com>
2022-11-28 13:27:50 +0000

    general info

--- src/objects/metric_tables.py
+++ src/objects/metric_tables.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from src.html.image_html import HTMLImageBuilder
+from src.objects.metric_obj import NumberOfCodeLinesHistogram, TypeMetricValueCountsTable, GeneralItemMetricHTMLTable, \
+    FunctionArgsHistogram
 
 plt.style.use('bmh')
 
@@ -30,53 +32,14 @@ class AllMetricsTable(HTMLTableObject):
         return AllMetricsDataframe().data()
 
 
-class AllMetricsStatsObj(HTMLObject):
+class AllMetricsStatsHTML(HTMLObject):
     def build(self):
         page = HTMLPage()
-
-        self._df_all_stats = AllMetricsDataframe().data()
-        [page.add(stats_html) for stats_html in self.number_stats()]
-
-    def number_stats(self):
-        res = []
-
-        df_numbers = self._df_all_stats.select_dtypes('number')
-        for col in df_numbers.columns:
-            df_metric = pd.concat([
-                self._df_all_stats[['item']],
-                df_numbers[[col]]],
-                axis=1
-            )
-
-            res.append(_hist(df_metric))
-            t = 1
-
-        exit()
-        return res
-
-
-def _hist(df):
-    x_col_name, y_col_name = df.columns[:2]
-
-    plt.figure()
-    plt.title(y_col_name)
-    plt.hist(df[y_col_name], bins=50)
-    # res_dict = plt.boxplot(df[y_col_name], showmeans=True)
-    # print(res_dict)
-    # print(res_dict['fliers'])
-    # print(res_dict['fliers'][0].get_data())
-    # print(dir(res_dict['fliers'][0].get_xdata()))
-    plt.yscale('log')
-    plt.grid()
-    plt.show()
-    return _plt_fig_to_image_html()
-
-
-def _plt_fig_to_image_html():
-    with tempfile.NamedTemporaryFile(suffix='_image.png') as tmp_file:
-        plt.savefig(tmp_file.name)
-        return HTMLImageBuilder(tmp_file.name).html
+        page.add(GeneralItemMetricHTMLTable().data())
+        page.add(NumberOfCodeLinesHistogram().data())
+        page.add(FunctionArgsHistogram().data())
+        return page.html
 
 
 if __name__ == '__main__':
-    AllMetricsStatsObj().data()
+    AllMetricsStatsHTML().data()
