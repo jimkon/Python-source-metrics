38683d35a6b5a7c5b41a1eb811a024c7ce0a9b5b
jimkon <dkontzedakis@gmail.com>
2023-03-16 22:50:27 +0000

    enable downloading the pages or a full report

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 from pystruct.objects.imports_data_objects import MostImportedPackages, UnusedModules, InvalidImports, \
     MostImportedProjectModules, MostImportedProjectPackages, ImportsStatsHTML
@@ -20,7 +21,14 @@ def main():
 def obj(obj_class):
     cls = getattr(sys.modules[__name__], obj_class)
     html_object = cls().data()
-    return render_template('objects.html', html_object=html_object)
+    return render_template('objects.html', **locals())
+
+
+@app.route('/download/<obj_class>')
+def download_obj(obj_class):
+    filepath = os.path.join(app.root_path, 'report_files/objs/', obj_class)
+    app.logger.info(f"{filepath}")
+    return send_file(filepath, as_attachment=True)
 
 
 if __name__ == "__main__":
--- pystruct/objects/full_report.py
+++ pystruct/objects/full_report.py
@@ -1,17 +1,13 @@
 from pystruct.html_utils.html_pages import HTMLPage, TabsHTML
-from pystruct.objects.data_objects import AbstractObject
+from pystruct.objects.data_objects import AbstractObject, HTMLObject
 from pystruct.objects.imports_data_objects import MostImportedPackages, UnusedModules, InvalidImports, \
     MostImportedProjectModules, MostImportedProjectPackages, ImportsStatsHTML
 from pystruct.objects.metric_tables import AllMetricsTable, AllMetricsStatsHTML
 from pystruct.objects.uml_graph_obj import UMLClassDiagramObj, UMLClassRelationDiagramObj, InProjectImportModuleGraphObj, \
     PackagesImportModuleGraphObj
-from pystruct.utils.file_strategies import HTMLFile
 
 
-class FullReport(AbstractObject):
-    def __init__(self):
-        super().__init__(HTMLFile(self))
-
+class FullReport(HTMLObject):
     def build(self):
         html_builder = TabsHTML()
         html_builder.add_tab("General info", AllMetricsStatsHTML().data())
