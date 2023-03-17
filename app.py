f329235a8c480175bb7938385d17c57f5f49cb80
jimkon <dkontzedakis@gmail.com>
2023-03-17 16:51:50 +0000

    load project page and functionality - fix error message

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def download_obj(obj_class):
 @app.route('/load/', methods=['GET', 'POST'])
 def load_project():
     app.logger.info(f"LOAD_PROJECT> {request.method}")
-    filepath_error, giturl_error = '', ''
+    error_message = ''
     source = None
     if request.method == 'POST':
         if "filepath_input" in request.form:
@@ -44,14 +44,10 @@ def load_project():
         elif "giturl_input" in request.form:
             source = request.form['giturl_input']
 
-        app.logger.info(f"LOAD_PROJECT> {request.form=}")
-        app.logger.info(f"LOAD_PROJECT> {source=}")
         try:
             grab_code(source)
         except Exception as e:
-            filepath_error = str(e)
-
-        app.logger.info(f"LOAD_PROJECT> {filepath_error}")
+            error_message = str(e)
 
     return render_template('load_project.html', **locals())
 
