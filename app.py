d4e9ea0e3e286c56215410098d331582923db24e
jimkon <dkontzedakis@gmail.com>
2023-05-13 21:23:22 +0100

    feature: rebuild all objects page for easy debugging

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import os
+import pandas as pd
+from flask import Flask, redirect, render_template, request, send_file, url_for, escape
 
-from flask import Flask, redirect, render_template, request, send_file, url_for
-
-from pystruct.plat.dataset_controller import DatasetController
 from pystruct.objects.full_report import FullReport
+from pystruct.plat.dataset_controller import DatasetController
 from pystruct.utils.object_utils import get_all_concrete_object_classes
 from pystruct.utils.object_utils import get_object_class_from_class_name
 
@@ -22,14 +21,6 @@ def main():
 
     table_of_content_dict = {k: v.__name__ for k, v in FullReport.content_dict.items()}
     return render_template('index.html', **locals(), **globals())
-
-
-@app.route('/debug')
-def debug():
-    existing_objs = sorted([obj.stem for obj in dataset_controller.current_dataset.objects_directory.rglob('*') if obj.is_file()])
-    all_objects = sorted([_cls.name() for _cls in get_all_concrete_object_classes()])
-    return render_template('debug.html', **locals(), **globals())
-
 
 
 @app.route('/obj/<obj_class_name>')
@@ -76,6 +67,38 @@ def new_project():
             error_message = str(e)
 
     return render_template('new_project.html', **locals(), **globals())
+
+
+@app.route('/debug')
+def debug():
+    existing_objs = sorted([obj.stem for obj in dataset_controller.current_dataset.objects_directory.rglob('*') if obj.is_file()])
+    all_objects = sorted([_cls.name() for _cls in get_all_concrete_object_classes()])
+    return render_template('debug.html', **locals(), **globals())
+
+
+@app.route('/debug/test_all_objects')
+def test_all_objects():
+    all_objects = [_cls for _cls in get_all_concrete_object_classes()]
+
+    results = []
+    for obj_class in all_objects:
+        try:
+            _cls = get_object_class_from_class_name(obj_class.name().replace(' ', ''))
+            result = _cls().build()
+        except Exception as e:
+            build_result = f"{e.__class__.__name__}: {str(e)}"
+        else:
+            build_result = f"Success (len={len(result)})"
+
+        obj_res = {
+            'object': f"<a href={url_for('build_obj', obj_class_name=obj_class.name())}>{obj_class.name()}</a>",
+            'build result': build_result
+        }
+        results.append(obj_res)
+
+    results_df = pd.DataFrame(results).sort_values('build result', ascending=True)
+    objects_table = results_df.to_html(escape=False)
+    return render_template('test_all_objects.html', **locals(), **globals())
 
 
 if __name__ == "__main__":
