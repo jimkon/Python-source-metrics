edd0b97d68dc65e80ac50c7907dcb441dfa591b6
dkontzedakis <dkontzedakis@deloitte.co.uk>
2023-06-07 21:54:25 +0100

    class method objects

--- pystruct/objects/imports_data_objects.py
+++ pystruct/objects/imports_data_objects.py
@@ -5,7 +5,44 @@ from pystruct.metrics.import_metrics import enrich_import_raw_df
 from pystruct.objects.data_objects import DataframeObjectABC, HTMLTableObjectABC, HTMLObjectABC
 from pystruct.objects.metric_obj import IsScriptFile
 from pystruct.objects.python_object import PObject
+from pystruct.python.code_structure import ClassMethodObj
 from pystruct.reports.import_graph import CollectImportsVisitor
+from pystruct.visitors.visitor import TreeNodeVisitor
+
+
+class ClassMethodsRawDataframe(DataframeObjectABC):
+    class CollectClassMethodsVisitor(TreeNodeVisitor):
+        def __init__(self):
+            self._class_methods_list = []
+
+        def visit_class_method(self, node):
+            class_name = node.parent.data.name
+            method = node.data.name.split('.')[-1]
+            self._class_methods_list.append({'class': class_name, 'method': method})
+
+        def to_dataframe(self):
+            return pd.DataFrame(self._class_methods_list, columns=['class', 'method'])
+
+    def build(self):
+        pobj = PObject().python_source_object()
+        class_methods = ClassMethodsRawDataframe.CollectClassMethodsVisitor()
+        pobj.use_visitor(class_methods)
+        return class_methods.to_dataframe()
+
+
+class ClassMethodsEnrichedDataframe(DataframeObjectABC):
+    def build(self):
+        df = ClassMethodsRawDataframe().dataframe()
+        self.enrich_df(df)
+        return df
+
+    def enrich_df(self, df):
+        df['visibility'] = df['method'].apply(lambda method: 'private' if method[:2] == '__'
+                                                        else 'protected' if method[:1] == '_' else 'public')
+
+
+if __name__ == '__main__':
+    ClassMethodsEnrichedDataframe().dataframe()
 
 
 class ImportsRawDataframe(DataframeObjectABC):
